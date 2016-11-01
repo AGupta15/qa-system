cbace13312af2127be614fc3e651a2932432b462
Evan King <esk79@cornell.edu>
2016-11-01 19:30:50 -0400

    added get answer from range

--- Part1.py
+++ Part1.py
@@ -1,9 +1,37 @@
 import os
 from sets import Set
-import sys
-import re
+import unirest
+
+ANSWER_TYPE = {
+    "who" : "PERSON",
+    "where" : "GPE",
+    "when" : "DATE"
+}
 
 path = "/Users/abhigupta/cs4740/qa-system/doc_dev"
+
+# determine the answer from a single range of words with specific answer_type
+def parse_answer_from_single_range(answer_type, range_tuple, words):
+    text = ' '.join(words[range_tuple[0]:range_tuple[1]])
+    response = unirest.post("https://textanalysis.p.mashape.com/spacy-named-entity-recognition-ner",
+                        headers={"X-Mashape-Key": "<key>", "Content-Type": "application/x-www-form-urlencoded",
+                                 "Accept": "application/json"}, params={"text": text})
+    for result in response.body["result"]:
+        if ANSWER_TYPE[answer_type] in result:
+            return result.rsplit('/')[0]
+
+# return a list of results for each of the given range tuples in range_tuples list
+# Example print parse_answer_from_ranges("who", [(1,12), (2,10), (6,16)], ["test", "My", "name", "is", "Evan", "and", "I", "can", "not", "live", "in", "California", "his", "name", "is", "Bob"])
+def parse_answer_from_ranges(answer_type, range_tuples, words):
+    result = []
+    for ranges in range_tuples:
+        current = parse_answer_from_single_range(answer_type, ranges, words)
+        if current not in result:
+            result.append(current)
+        if len(result) == 5:
+            break
+    return result
+
 
 def makeWordList(directory):
     wordList = []
@@ -23,7 +51,6 @@ def makeWordList(directory):
 
 
 def findWordRanges(wordList, directory):
-
     dirWordList = makeWordList(directory)
     print dirWordList
 
@@ -44,7 +71,8 @@ def findWordRanges(wordList, directory):
     return rangeArray
 
 
-
 fileWordList = makeWordList(89)
 wordArray = ["the", "hello", "hi"]
 findWordRanges(wordArray, 89)
+
+
