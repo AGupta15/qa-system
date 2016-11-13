8273a3fe5dd2922c16f964c5c6c4d42cf3a835c2
Evan King <esk79@cornell.edu>
2016-11-13 16:23:15 -0500

    final

--- Part1.py
+++ Part1.py
@@ -1,16 +1,17 @@
 import os
 from sets import Set
+from difflib import SequenceMatcher
 import unirest
-import nltk
 
-API_KEY = "y5TSV5GnovmshgN9RBrmSvGlOT7Lp1bSp3xjsnaJQ5nmWCtG0H"
-SIM_THRESHOLD = .5
+API_KEY="WZ0ualun6QmshdQduPj4uBwILMncp17djDFjsnlJb4JOqNT6sU"
+
+
 ANSWER_TYPE = {
-    "who": ["PERSON"],
-    "where": ["GPE", "LOC"],
-    "when": ["DATE"]
+    "who" : ["PERSON"],
+    "where" : ["GPE","LOC"],
+    "when" : ["DATE"]
 }
-
+SIM_THRESHOLD = .70
 SENTENCE_TERMINALS = ['.', '!', '?', '<', '>', '/']
 QUESTION_FILE="question.txt"
 SMALL_QUESTION_FILE="smallq.txt"
@@ -27,34 +28,32 @@ COMMONLY_USED_WORDS=set(["the", "be", "to", "of", "and", "a", "in", "that","have
 
 path = "doc_dev"
 
-
 # string of format "questionnumber questionidentifier keyword1 keyword2... keywordn"
 # takes a string and filters the commonly used words out, returns a tuple of
 # the question_number, question_identifier, and keywords
-def filterInputQuestion(question_string, bad_words):
-    keywords = filter(lambda x: not x in bad_words, question_string.split())
-    question_number = keywords.pop(0)
-    question_identifier = keywords.pop(0)
-    if question_identifier == "Where's":
-        question_identifier = "Where"
-    if question_identifier == "Who's":
-        question_identifier = "Who"
-    return (question_number, question_identifier, keywords)
-
+def filterInputQuestion(question_string,bad_words):
+  keywords=filter(lambda x: not x in bad_words, question_string.split())
+  question_number=keywords.pop(0)
+  question_identifier=keywords.pop(0)
+  if question_identifier=="Where's":
+    question_identifier="Where"
+  if question_identifier=="Who's":
+    question_identifier="Who"
+  return (question_number,question_identifier,keywords)
 
 # determine the answer from a single range of words with specific answer_type
 def parse_answer_from_single_range(answer_type, range_tuple, words, question_keys):
     text = ' '.join(words[range_tuple[0]:range_tuple[1]])
     print "text: {}".format(text)
     response = unirest.post("https://textanalysis.p.mashape.com/spacy-named-entity-recognition-ner",
-                            headers={"X-Mashape-Key": API_KEY, "Content-Type": "application/x-www-form-urlencoded",
-                                     "Accept": "application/json"}, params={"text": text})
-    if response.headers.get('X-RateLimit-requests-Remaining') < 10: raise Exception('Dont waste Alexs Money!!' + str(response.headers))
+                        headers={"X-Mashape-Key": API_KEY, "Content-Type": "application/x-www-form-urlencoded",
+                                 "Accept": "application/json"}, params={"text": text})
+    if response.headers.get('X-RateLimit-requests-Remaining') < 10: raise Exception('Dont waste Alexs Money!!')
     answer = []
     for result in response.body["result"]:
-        token = result.rsplit('/')
-        if token[1] in ANSWER_TYPE[answer_type] and not token[0] in question_keys:
-            answer.append((token[0], (range_tuple[0] + range_tuple[1]) / 2))
+      token=result.rsplit('/')
+      if token[1] in ANSWER_TYPE[answer_type] and not token[0] in question_keys:
+        answer.append((token[0], (range_tuple[0] + range_tuple[1]) / 2))
     return answer
 
 # return a list of results for each of the given range tuples in range_tuples list
@@ -65,79 +64,81 @@ def parse_answer_from_ranges(answer_type, range_tuples, words, question_keys):
         answers = parse_answer_from_single_range(answer_type, ranges, words, question_keys)
         if answers:
             for current in answers:
-                if current not in result:
+                if not any(result[0] == current[0] for result in result):
                     result.append(current)
                 if len(result) == 5:
                     return result
     return result
 
+def are_similar(word1,word2):
+    return SequenceMatcher(None, word1, word2).ratio() >= .8
 
 # makes a list of words in a directory
 def makeWordList(directory):
     wordList = []
-    dirPath = path + "/" + str(directory) + "/"
-    file_list = os.listdir(dirPath)
-    file_list = [f for f in file_list if f[0] != '.']
-    file_start_ends = [0]
-    counter = 0
+    dirPath = path +"/"+ str(directory)+"/"
+    file_list=os.listdir(dirPath)
+    file_list=[f for f in file_list if f[0] != '.']
+    file_start_ends=[0]
+    counter=0
     for file_obj in file_list:
-        filePath = os.path.join(dirPath, file_obj)
+        filePath=os.path.join(dirPath, file_obj)
         f = open(filePath)
         for line in f:
-          values=nltk.word_tokenize(line)
+          values=line.split()
           for value in values:
             counter+=1
             wordList.append(value)
         file_start_ends.append(counter)
 
-    return wordList, file_start_ends
-
+    return wordList,file_start_ends
 
 # finds the ranges that the list of keywords appears in
 def findWordRanges(wordList, directory):
     dirWordList = makeWordList(directory)[0]
-    # print dirWordList
+    map = {}
     question_words = []
     for word in wordList:
-        question_words.add(word.lower())
+        question_words.append(word.lower())
 
     rangeArray = []
     for i in range(0, len(dirWordList)):
-        if dirWordList[i].lower() in question_words or any(get_similarity(dirWordList[i].lower(), word) > SIM_THRESHOLD for word in question_words):
+        # print i
+        current = dirWordList[i].lower()
+        if current not in map and current not in COMMONLY_USED_WORDS:
+            map[current] = any(
+                are_similar(current, word) > SIM_THRESHOLD for word in question_words)
+            should_add = map[current]
+
+        if current not in COMMONLY_USED_WORDS and should_add:
             rangeArray.append(i)
     return rangeArray
 
 
-
 # Evan cluster
-
 def cluster_func(indices_list, word_list, max_gap = 30):
-  indices_list.sort()
-  groups = [[indices_list[0]]]
-  for x in indices_list[1:]:
-      if abs(x - groups[-1][-1]) <= max_gap:
-          groups[-1].append(x)
-      else:
-          groups.append([x])
-  groups.sort(key=len, reverse=True)
+    indices_list.sort()
+    groups = [[indices_list[0]]]
+    for x in indices_list[1:]:
+        if abs(x - groups[-1][-1]) <= max_gap:
+            groups[-1].append(x)
+        else:
+            groups.append([x])
+    groups.sort(key=len, reverse=True)
 
-  groups = [group for group in groups if len(group) >= 2]
-  # adjust chunks to extend to full sentences on each side of the bound
-  for group in groups:
-    first = group[0]
-    last = group[-1]
-    while (first > 0 and not any(terminal in word_list[first] for terminal in SENTENCE_TERMINALS)):
-      first -= 1
-    while (last <= (len(word_list) - 1) and not any(terminal in word_list[last] for terminal in SENTENCE_TERMINALS)):
-      last += 1
-    group[0] = first + 1
-    group[-1] = last - 1
-  return groups
-
-def get_similarity(word1, word2):
-    response = unirest.get("http://swoogle.umbc.edu/SimService/GetSimilarity?operation=api&phrase1={}&phrase2={}".format(word1, word2))
-    try: return float(response.body)
-    except ValueError: return None
+    groups = [group for group in groups if len(group) >= 2]
+    # adjust chunks to extend to full sentences on each side of the bound
+    for group in groups:
+        first = group[0]
+        last = group[-1]
+        while (first > 0 and not any(terminal in word_list[first] for terminal in SENTENCE_TERMINALS)):
+            first -= 1
+        while (last <= (len(word_list) - 1) and not any(
+                    terminal in word_list[last] for terminal in SENTENCE_TERMINALS)):
+            last += 1
+        group[0] = first + 1
+        group[-1] = last - 1
+    return [(x[0], x[-1]) for x in groups]
 
 # uses the list of tuples and list of file_starts to find where a density occurs
 def find_relevant_doc_id(average, file_start_ends):
@@ -145,53 +146,40 @@ def find_relevant_doc_id(average, file_start_ends):
         if average >= file_start_ends[file_start_index - 1] and average <= file_start_ends[file_start_index]:
             return file_start_index
 
-
 # takes a file location which is the file of raw questions
 # parses all the questions and for each one, calls filterInputQuestion
 def parseAllQuestions(question_location):
-    question_file = open(question_location)
-    raw_data = question_file.read()
-    questions = raw_data.replace("<top>\r\n\r\n<num> Number: ", '') \
-        .replace('\r\n\r\n<desc> Description:\r\n', ' ').replace('?\r\n\r\n<', '') \
-        .replace('top>\r\n\r\n\r\n', '').split('/')
-    # print questions
+  question_file=open(question_location)
+  raw_data=question_file.read()
+  questions=raw_data.replace("<top>\r\n\r\n<num> Number: ",'') \
+    .replace('\r\n\r\n<desc> Description:\r\n',' ').replace('?\r\n\r\n<','') \
+    .replace('top>\r\n\r\n\r\n','').split('/')
+  print questions
 
-    output_file = open("answer.txt", 'w')
-    for question_string in questions[:-1]:
-        print question_string
-        question_data = filterInputQuestion(question_string, COMMONLY_USED_WORDS)
-        question_id = question_data[0]
-        # find word ranges for the keywords and corresponding question number
-        word_ranges = findWordRanges(question_data[2], question_id)
-        # print word_ranges
-        word_list_and_ranges = makeWordList(question_id)
-        word_list = word_list_and_ranges[0]
-        file_start_ends = word_list_and_ranges[1]
-        # print file_start_ends
-        # range_tuples are indices into the word_ranges array
-        range_tuples = cluster_func(word_ranges, word_list)
+  output_file=open("answer.txt",'w')
+  for question_string in questions[:-1]:
+    print question_string
+    question_data= filterInputQuestion(question_string,COMMONLY_USED_WORDS)
+    question_id=question_data[0]
+    # find word ranges for the keywords and corresponding question number
+    word_ranges=findWordRanges(question_data[2],question_id)
+    # print word_ranges
+    word_list_and_ranges=makeWordList(question_id)
+    word_list=word_list_and_ranges[0]
+    file_start_ends=word_list_and_ranges[1]
+    # print file_start_ends
+    # range_tuples are indices into the word_ranges array
+    range_tuples=cluster_func(word_ranges, word_list)
 
-        first_five = range_tuples[:5]
-        print first_five
+    first_five=range_tuples[:5]
 
-        # output answers to the answers.txt file
-        list_of_answers = parse_answer_from_ranges(question_data[1].lower(), first_five, word_list, question_data[2])
-        # list_of_answers= [("1",2),("2",2),("3",1000),("4",2000),("5",500)]
-        counter = 0
-        for answer in list_of_answers:
-            print answer[0]
-            # nothing returned
-            if (answer[0] == "None"):
-                output_file.write(str(question_id) + ' ' + '1' + ' ' + ' ')
-                # answer found
-            else:
-                doc_id=find_relevant_doc_id(answer[1],file_start_ends)
-                print "DOCUMENT ID for average "+str(answer[1])+": "+str(doc_id)
-                output_file.write(str(question_id) + ' ' + str(doc_id) + ' ' + str(answer) + '\n')
-            counter += 1
-
-
+    # output answers to the answers.txt file
+    list_of_answers= parse_answer_from_ranges(question_data[1].lower(), first_five, word_list,question_data[2])
+    # list_of_answers= ["1","2","3","4","5"]
+    for answer in list_of_answers:
+      print answer[0]
+      # nothing returned
+      output_file.write(str(question_id)+' '+str(find_relevant_doc_id(answer[1], file_start_ends))+' '+str(answer[0]) +'\n')
 
 # starts calling all questions
-# parseAllQuestions(SMALL_QUESTION_FILE)
-get_similarity('kill','murder')
+parseAllQuestions(SMALL_QUESTION_FILE)
